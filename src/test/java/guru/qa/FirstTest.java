bdd5b4e8b4717af297a0e534ad47b8bd496dce28
LizaQA <LizaQA@gmail.com>
2022-06-03 01:34:46 +0300

    change operator to >=

--- src/test/java/guru/qa/FirstTest.java
+++ src/test/java/guru/qa/FirstTest.java
@@ -7,12 +7,12 @@ public class FirstTest {
 
     @Test
     public void firstTest() {
-        Assertions.assertTrue(3 > 2, "Неверно");
+        Assertions.assertTrue(3 >= 2, "Неверно");
     }
 
     @Test
     public void secondTest() {
-        Assertions.assertTrue(3 > 1, "Неверно");
+        Assertions.assertTrue(3 >= 1, "Неверно");
     }
 }
 
